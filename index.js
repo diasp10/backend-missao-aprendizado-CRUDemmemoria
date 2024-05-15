59cd4a8a1b0d590bd6193b31c1272fb2319be95a
diasp10 <gustavo.creep@gmail.com>
2024-05-15 20:35:19 +0000

    Implementacao do create `[POST] /personagem`

--- index.js
+++ index.js
@@ -22,11 +22,28 @@ app.get(`/personagem/:id`, function(req, res){
     //Acesso item na lista usando id -1
     const item = lista[id - 1]
 
-    //enviamos o item como resposta http://localhost:3000/personagem/2
+    //enviamos o item como resposta http://localhost:3000/personagem
     res.send(item)
 } )
 
+//Sinalizo para express que estamos usando json no body
+app.use(express.json())
 
+//endpoint create [POST] /personagem
+app.post(`/personagem`, function(req, res){
+
+    //acessamos o body da requisicao
+    const body = req.body
+		//Acessamos a propriedade `nome` do body
+    const Novoitem = body.nome
+    console.log(body)
+
+    //Adicionamos na lista
+		lista.push(Novoitem)
+
+    res.send(`Item adicionado com sucesso: `+ Novoitem)
+}
+)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
