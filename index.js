8be3fbef8ec2b23f6b59c0e6cd294e9b6c96cf78
diasp10 <gustavo.creep@gmail.com>
2024-05-15 19:44:18 +0000

    Endpont `read all [GET] /personagem implementado

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ app.get('/', (req, res) => {
   res.send('Hello World!!')
 })
 
+const lista = ['Java', 'Ketlin', 'Android']
+
+//read all get (personagem)
+
+app.get('/personagem', function(req, res){
+    res.send(lista)
+})
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
