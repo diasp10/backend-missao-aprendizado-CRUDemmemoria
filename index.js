74cb9f6cb2f39547bec56ad0fff350160ee346d9
diasp10 <gustavo.creep@gmail.com>
2024-05-20 08:23:02 +0000

    Status HTTP adicionados em POST e PUT

--- index.js
+++ index.js
@@ -23,14 +23,6 @@ app.get(`/personagem/:id`, function(req, res){
     const item = lista[id - 1]
     //enviamos o item como resposta http://localhost:3000/personagem/1
 
-    if (!novoItem){
-      return res.send(`Corpo da requisicao deve estar presente no body`)
-    }
-    //Checar se o novo item esta presente ou nao
-    if(lista.includes(novoItem)){
-      return res.send(`Esse item ja existe na lista`)
-    }
-
     res.send(item)
 } )
 
@@ -47,17 +39,17 @@ app.post(`/personagem`, function(req, res){
     console.log(body)
     //Checar se o nome esta presente no body
     if (!novoItem){
-      return res.send(`Corpo da requisicao deve estar presente no body`)
+      return res.status(400).send(`Corpo da requisicao deve estar presente no body`)
     }
     //Checar se o novo item esta presente ou nao
     if(lista.includes(novoItem)){
-      return res.send(`Esse item ja existe na lista`)
+      return res.status(409).send(`Esse item ja existe na lista`)
     }
 
     //Adicionamos na lista
 		lista.push(novoItem)
 
-    res.send(`Item adicionado com sucesso: `+ novoItem)
+    res.status(201).send(`Item adicionado com sucesso: `+ novoItem)
 })
 
 //endpoint update[put] /personagem/:id
@@ -72,14 +64,14 @@ app.put('/personagem/:id', function (req, res) {
   lista[id - 1] = novoItem
 
   if (!novoItem){
-    return res.send(`Corpo da requisicao deve estar presente no body`)
+    return res.status(400).send(`Corpo da requisicao deve estar presente no body`)
   }
   //Checar se o novo item esta presente ou nao
   if(lista.includes(novoItem)){
-    return res.send(`Esse item ja existe na lista`)
+    return res.status(409).send(`Esse item ja existe na lista`)
   }
 
-  res.send('Item atualizado com sucesso: ' + id + ' - ' + novoItem)
+  res.status(201).send('Item atualizado com sucesso: ' + id + ' - ' + novoItem)
 })
 
 app.delete('/personagem/:id', function (req, res){
